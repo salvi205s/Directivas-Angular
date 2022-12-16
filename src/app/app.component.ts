b272d67902eb9ad2c7c5a9ce8730d59618a6fb70
salvi205 <salvi205s@gmail.com>
2022-12-16 10:41:56 +0100

    Directivas angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'trabaDirectivaApp';
+  titulo = 'Registro de usuarios';
+  mensaje = '';
+  registrado = false;
+  nombre: String = '';
+  apellido: String = '';
+  entradas: any;
+
+  constructor() {
+    this.entradas = [
+      { titulo: 'Python cada dia mas presente' },
+      { titulo: 'Java presente desde hace mas de 20 años' },
+      { titulo: 'JavaScript cada dia mas funcional ' },
+      { titulo: 'Kotlin potencia para tus apps' },
+      { titulo: 'Donde quedo pascal?' },
+    ];
+  }
+
+  registrarUsuario() {
+    this.registrado = true;
+
+    this.mensaje = 'Usuario registrado con exito';
+  }
 }
